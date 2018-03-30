b8faeeaab6bfc3ed5ccb398e456dcfc029843218
ayudhhande <ayudhsanjay@gmail.com>
2018-03-30 22:31:28 +0530

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ var UserSchema = new mongoose.Schema({
             return token;
         });
     };
+
+    UserSchema.methods.removeToken = function(token){
+        var user = this;
+
+        return user.update({
+            $pull:{
+                tokens: {token}
+            }
+        });
+    }
     
     UserSchema.statics.findByToken = function(token){
         var User = this;
